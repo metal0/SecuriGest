7f7809136072bf817e208db7c7c0e5e6d68af645
martioliveira <48357829+martioliveira@users.noreply.github.com>
2019-05-17 11:59:50 +0100

    Correção de erro na build de produção

--- src/app/rooms/room-list/room-list.component.ts
+++ src/app/rooms/room-list/room-list.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { RoomsServiceService } from '../rooms-service.service';
 @Component({
-  selector: 'app-room-list',
-  templateUrl: './room-list.component.html',
-  styleUrls: ['./room-list.component.css']
+	selector: 'app-room-list',
+	templateUrl: './room-list.component.html',
+	styleUrls: [
+		'./room-list.component.css'
+	]
 })
 export class RoomListComponent implements OnInit {
+	constructor(public service: RoomsServiceService) {}
 
-  constructor(private service : RoomsServiceService) { }
-
-  ngOnInit() {
-  }
-
+	ngOnInit() {}
 }
--- src/app/rooms/rooms-service.service.ts
+++ src/app/rooms/rooms-service.service.ts
@@ -2,21 +2,21 @@ import { Injectable } from '@angular/core';
 import { Room } from './room.model';
 import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class RoomsServiceService {
- formData : Room;
- roomList = [];
+	formData: Room;
+	roomList = [];
 
- private roomsUrl = `${document.location.origin}/api/rooms`;
- private roomUrl = `${document.location.origin}/api/room`;
-  
-  constructor( private http: HttpClient) { }
+	private roomsUrl = `${document.location.origin}/api/rooms`;
+	private roomUrl = `${document.location.origin}/api/room`;
 
-  submitRoom(){
-    return this.http.post<any>(this.roomUrl,this.formData);
-  };
-  refreshList(){
-    return this.http.get<any>(this.roomsUrl);
-  };
+	constructor(private http: HttpClient) {}
+
+	submitRoom() {
+		return this.http.post<any>(this.roomUrl, this.formData);
+	}
+	refreshList() {
+		return this.http.get<any>(this.roomsUrl);
+	}
 }
