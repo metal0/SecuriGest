da0b9ff8f4848abffbac3851d8827f5e96d2279c
Tiago Eusébio <tiago.1998.eusebio@gmail.com>
2019-05-24 12:11:37 +0100

    Correções

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { AuthGuard } from './auth.guard';
 import { TokenInterceptorService } from './token-interceptor.service';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MaterialModule} from './material/material.module';
+import { MaterialModule } from './material/material.module';
 import { RoomComponent } from './rooms/room/room.component';
 import { RoomsComponent } from './rooms/rooms.component';
 import { RoomListComponent } from './rooms/room-list/room-list.component';
@@ -29,40 +29,55 @@ import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { CalendarProfileComponent } from './calendar-profile/calendar-profile.component';
-import { EntriesExitsComponent } from './entries-exits/entries-exits.component';
 
 @NgModule({
-	declarations: [AppComponent, LoginComponent, RegisterComponent, UsersComponent, HomeComponent, RecoverComponent, RoomComponent, RoomsComponent, RoomListComponent, PerfilComponent, CalendarProfileComponent, EntriesExitsComponent],
+	declarations: [
+		AppComponent,
+		LoginComponent,
+		RegisterComponent,
+		UsersComponent,
+		HomeComponent,
+		RecoverComponent,
+		RoomComponent,
+		RoomsComponent,
+		RoomListComponent,
+		PerfilComponent,
+		CalendarProfileComponent
+	],
 	imports: [
-	BrowserModule,
-	AppRoutingModule,
-	ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-	NgbModule.forRoot(),
-	FormsModule,
-	HttpClientModule,
-	BrowserAnimationsModule,
-	MaterialModule,
-	FormsModule, 
-	FlatpickrModule.forRoot(),
-	CommonModule,
-    NgbModalModule,
-    FlatpickrModule.forRoot(),
-    CalendarModule.forRoot({
-      provide: DateAdapter,
-      useFactory: adapterFactory
-    })
+		BrowserModule,
+		AppRoutingModule,
+		ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+		NgbModule.forRoot(),
+		FormsModule,
+		HttpClientModule,
+		BrowserAnimationsModule,
+		MaterialModule,
+		FormsModule,
+		FlatpickrModule.forRoot(),
+		CommonModule,
+		NgbModalModule,
+		FlatpickrModule.forRoot(),
+		CalendarModule.forRoot({
+			provide: DateAdapter,
+			useFactory: adapterFactory
+		})
 	],
 	providers: [
-	AuthService,
-	AuthGuard,
-	UserService,
-	{
-	provide: HTTP_INTERCEPTORS,
-	useClass: TokenInterceptorService,
-	multi: true
-	}
+		AuthService,
+		AuthGuard,
+		UserService,
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: TokenInterceptorService,
+			multi: true
+		}
 	],
-	bootstrap: [AppComponent],
-	exports: [CalendarProfileComponent]
-	})
+	bootstrap: [
+		AppComponent
+	],
+	exports: [
+		CalendarProfileComponent
+	]
+})
 export class AppModule {}
