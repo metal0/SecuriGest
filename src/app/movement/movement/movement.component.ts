0af5ae72633a1dcbfa2fc00b23691e084aa96cd8
Tiago Eusébio <tiago.1998.eusebio@gmail.com>
2019-08-01 17:08:26 +0100

    arranjar import httpclient

--- src/app/movement/movement/movement.component.ts
+++ src/app/movement/movement/movement.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, NgForm, NgModel } from '@angular/forms';
 import { Router } from '@angular/router';
-import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
 	selector: 'app-movement',
